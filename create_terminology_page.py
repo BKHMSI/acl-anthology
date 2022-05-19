63eb3b71b5ace9863faa0eaa1e8356fc6453e3ab
Badr Alkhamissy <bkhmsi@fb.com>
2022-05-19 00:10:10 -0700

    hyperlinked terms

--- create_terminology_page.py
+++ create_terminology_page.py
@@ -1,9 +1,14 @@
-from asyncore import write
 import json 
+from tqdm import tqdm 
 
 def read_jsonl(path):
     with open(path, 'r') as fin:
         data = [json.loads(line.strip()) for line in fin.readlines()]
+    return data 
+
+def read_json(path):
+    with open(path, 'r') as fin:
+        data = json.load(fin)
     return data 
 
 def write_json(path, data):
@@ -14,6 +19,22 @@ def write_json(path, data):
 if __name__ == "__main__":
 
     read_path = "./data/terms.json"
-    write_json(read_path, read_jsonl(read_path))
+    save_path = "./data/terms_links.json"
+
+    # write_json(read_path, read_jsonl(read_path))
+
+    data = read_json(read_path)
+    for i, row in tqdm(enumerate(data), total=len(data)):
+        for sent in row["sentences"]:
+            if len(sent["wiki"]) == 0:
+                data[i]["wiki"] = ""
+            else:
+                data[i]["wiki"] = sent["wiki"][0][1]
+                break
+        if data[i]["wiki"] == "":
+            print(row["term"])
+        
+    
+    write_json(save_path, data)
 
     
