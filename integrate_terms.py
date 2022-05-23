c464ab8f1f13c1fd6b616445857410a448f36019
Badr Alkhamissy <bkhmsi@fb.com>
2022-05-22 21:04:27 -0700

    support for terms in 16 languages

---
+++ integrate_terms.py
@@ -0,0 +1,302 @@
+import os
+import json 
+from glob import glob
+from tqdm import tqdm 
+
+def read_jsonl(path):
+    with open(path, 'r') as fin:
+        data = [json.loads(line.strip()) for line in fin.readlines()]
+    return data 
+
+def read_json(path):
+    with open(path, 'r') as fin:
+        data = json.load(fin)
+    return data 
+
+def write_json(path, data):
+    with open(path, 'w') as fout:
+        json.dump(data, fout)
+    
+languages = ['Afrikaans', 'Albanian', 'Amharic', 'Arabic', 'Armenian', 'Azerbaijani', 'Bengali', 'Bosnian', 'Bulgarian', 'Catalan', 'Chinese', 'Croatian', 'Czech', 'Danish', 'Dutch', 'Estonian', 'Filipino', 'Finnish', 'French', 'Georgian', 'German', 'Greek', 'Hausa', 'Hebrew', 'Hindi', 'Hungarian', 'Icelandic', 'Indonesian', 'Irish', 'Italian', 'Japanese', 'Javanese', 'Kazakh', 'Korean', 'Lithuanian', 'Macedonian', 'Malay', 'Malayalam', 'Maltese', 'Mongolian', 'Norwegian', 'Persian', 'Polish', 'Portuguese', 'Romanian', 'Russian', 'Serbian', 'Sinhala', 'Slovak', 'Somali', 'Spanish', 'Swahili', 'Swedish', 'Tamil', 'Tibetan', 'Turkish', 'Ukranian', 'Urdu', 'Uzbek', 'Vietnamese']
+
+languages_codes = [
+    ('aa', 'Afar'),
+    ('ab', 'Abkhazian'),
+    ('af', 'Afrikaans'),
+    ('ak', 'Akan'),
+    ('sq', 'Albanian'),
+    ('am', 'Amharic'),
+    ('ar', 'Arabic'),
+    ('an', 'Aragonese'),
+    ('hy', 'Armenian'),
+    ('as', 'Assamese'),
+    ('av', 'Avaric'),
+    ('ae', 'Avestan'),
+    ('ay', 'Aymara'),
+    ('az', 'Azerbaijani'),
+    ('ba', 'Bashkir'),
+    ('bm', 'Bambara'),
+    ('eu', 'Basque'),
+    ('be', 'Belarusian'),
+    ('bn', 'Bengali'),
+    ('bh', 'Bihari languages'),
+    ('bi', 'Bislama'),
+    ('bo', 'Tibetan'),
+    ('bs', 'Bosnian'),
+    ('br', 'Breton'),
+    ('bg', 'Bulgarian'),
+    ('my', 'Burmese'),
+    ('ca', 'Catalan; Valencian'),
+    ('cs', 'Czech'),
+    ('ch', 'Chamorro'),
+    ('ce', 'Chechen'),
+    ('zh', 'Chinese'),
+    ('cu', 'Church Slavic; Old Slavonic; Church Slavonic; Old Bulgarian; Old Church Slavonic'),
+    ('cv', 'Chuvash'),
+    ('kw', 'Cornish'),
+    ('co', 'Corsican'),
+    ('cr', 'Cree'),
+    ('cy', 'Welsh'),
+    ('cs', 'Czech'),
+    ('da', 'Danish'),
+    ('de', 'German'),
+    ('dv', 'Divehi; Dhivehi; Maldivian'),
+    ('nl', 'Dutch; Flemish'),
+    ('dz', 'Dzongkha'),
+    ('el', 'Greek, Modern (1453-)'),
+    ('en', 'English'),
+    ('eo', 'Esperanto'),
+    ('et', 'Estonian'),
+    ('eu', 'Basque'),
+    ('ee', 'Ewe'),
+    ('fo', 'Faroese'),
+    ('fa', 'Persian'),
+    ('fj', 'Fijian'),
+    ('fi', 'Finnish'),
+    ('fr', 'French'),
+    ('fy', 'Western Frisian'),
+    ('ff', 'Fulah'),
+    ('fl', 'Filipino'),
+    ('Ga', 'Georgian'),
+    ('de', 'German'),
+    ('gd', 'Gaelic; Scottish Gaelic'),
+    ('ga', 'Irish'),
+    ('gl', 'Galician'),
+    ('gv', 'Manx'),
+    ('el', 'Greek'),
+    ('gn', 'Guarani'),
+    ('gu', 'Gujarati'),
+    ('ht', 'Haitian; Haitian Creole'),
+    ('ha', 'Hausa'),
+    ('he', 'Hebrew'),
+    ('hz', 'Herero'),
+    ('hi', 'Hindi'),
+    ('ho', 'Hiri Motu'),
+    ('hr', 'Croatian'),
+    ('hu', 'Hungarian'),
+    ('hy', 'Armenian'),
+    ('ig', 'Igbo'),
+    ('is', 'Icelandic'),
+    ('io', 'Ido'),
+    ('ii', 'Sichuan Yi; Nuosu'),
+    ('iu', 'Inuktitut'),
+    ('ie', 'Interlingue; Occidental'),
+    ('ia', 'Interlingua (International Auxiliary Language Association)'),
+    ('id', 'Indonesian'),
+    ('ik', 'Inupiaq'),
+    ('is', 'Icelandic'),
+    ('it', 'Italian'),
+    ('jv', 'Javanese'),
+    ('ja', 'Japanese'),
+    ('kl', 'Kalaallisut; Greenlandic'),
+    ('kn', 'Kannada'),
+    ('ks', 'Kashmiri'),
+    ('ka', 'Georgian'),
+    ('kr', 'Kanuri'),
+    ('kk', 'Kazakh'),
+    ('km', 'Central Khmer'),
+    ('ki', 'Kikuyu; Gikuyu'),
+    ('rw', 'Kinyarwanda'),
+    ('ky', 'Kirghiz; Kyrgyz'),
+    ('kv', 'Komi'),
+    ('kg', 'Kongo'),
+    ('ko', 'Korean'),
+    ('kj', 'Kuanyama; Kwanyama'),
+    ('ku', 'Kurdish'),
+    ('lo', 'Lao'),
+    ('la', 'Latin'),
+    ('lv', 'Latvian'),
+    ('li', 'Limburgan; Limburger; Limburgish'),
+    ('ln', 'Lingala'),
+    ('lt', 'Lithuanian'),
+    ('lb', 'Luxembourgish; Letzeburgesch'),
+    ('lu', 'Luba-Katanga'),
+    ('lg', 'Ganda'),
+    ('mk', 'Macedonian'),
+    ('mh', 'Marshallese'),
+    ('ml', 'Malayalam'),
+    ('mi', 'Maori'),
+    ('mr', 'Marathi'),
+    ('ms', 'Malay'),
+    ('Mi', 'Micmac'),
+    ('mk', 'Macedonian'),
+    ('mg', 'Malagasy'),
+    ('mt', 'Maltese'),
+    ('mn', 'Mongolian'),
+    ('mi', 'Maori'),
+    ('ms', 'Malay'),
+    ('my', 'Burmese'),
+    ('na', 'Nauru'),
+    ('nv', 'Navajo; Navaho'),
+    ('nr', 'Ndebele, South; South Ndebele'),
+    ('nd', 'Ndebele, North; North Ndebele'),
+    ('ng', 'Ndonga'),
+    ('ne', 'Nepali'),
+    ('nl', 'Dutch; Flemish'),
+    ('nn', 'Norwegian Nynorsk; Nynorsk, Norwegian'),
+    ('nb', 'Bokmål, Norwegian; Norwegian Bokmål'),
+    ('no', 'Norwegian'),
+    ('oc', 'Occitan (post 1500)'),
+    ('oj', 'Ojibwa'),
+    ('or', 'Oriya'),
+    ('om', 'Oromo'),
+    ('os', 'Ossetian; Ossetic'),
+    ('pa', 'Panjabi; Punjabi'),
+    ('fa', 'Persian'),
+    ('pi', 'Pali'),
+    ('pl', 'Polish'),
+    ('pt', 'Portuguese'),
+    ('ps', 'Pushto; Pashto'),
+    ('qu', 'Quechua'),
+    ('rm', 'Romansh'),
+    ('ro', 'Romanian; Moldavian; Moldovan'),
+    ('ro', 'Romanian; Moldavian; Moldovan'),
+    ('rn', 'Rundi'),
+    ('ru', 'Russian'),
+    ('sg', 'Sango'),
+    ('sa', 'Sanskrit'),
+    ('si', 'Sinhala; Sinhalese'),
+    ('sk', 'Slovak'),
+    ('sk', 'Slovak'),
+    ('sl', 'Slovenian'),
+    ('se', 'Northern Sami'),
+    ('sm', 'Samoan'),
+    ('sn', 'Shona'),
+    ('sd', 'Sindhi'),
+    ('so', 'Somali'),
+    ('st', 'Sotho, Southern'),
+    ('es', 'Spanish; Castilian'),
+    ('sq', 'Albanian'),
+    ('sc', 'Sardinian'),
+    ('sr', 'Serbian'),
+    ('ss', 'Swati'),
+    ('su', 'Sundanese'),
+    ('sw', 'Swahili'),
+    ('sv', 'Swedish'),
+    ('ty', 'Tahitian'),
+    ('ta', 'Tamil'),
+    ('tt', 'Tatar'),
+    ('te', 'Telugu'),
+    ('tg', 'Tajik'),
+    ('tl', 'Tagalog'),
+    ('th', 'Thai'),
+    ('bo', 'Tibetan'),
+    ('ti', 'Tigrinya'),
+    ('to', 'Tonga (Tonga Islands)'),
+    ('tn', 'Tswana'),
+    ('ts', 'Tsonga'),
+    ('tk', 'Turkmen'),
+    ('tr', 'Turkish'),
+    ('tw', 'Twi'),
+    ('ug', 'Uighur; Uyghur'),
+    ('uk', 'Ukranian'),
+    ('ur', 'Urdu'),
+    ('uz', 'Uzbek'),
+    ('ve', 'Venda'),
+    ('vi', 'Vietnamese'),
+    ('vo', 'Volapük'),
+    ('cy', 'Welsh'),
+    ('wa', 'Walloon'),
+    ('wo', 'Wolof'),
+    ('xh', 'Xhosa'),
+    ('yi', 'Yiddish'),
+    ('yo', 'Yoruba'),
+    ('za', 'Zhuang; Chuang'),
+    ('zh', 'Chinese'),
+    ('zu', 'Zulu')
+]
+
+lang_to_code = {}
+for code, langs in languages_codes:
+    for lang in langs.split(";"):
+        lang_to_code[lang.strip()] = code
+
+def clean_data(data):
+
+    cleaned_data = {}
+    old_term = data[0]["term"]
+    translated_term = data[0]["translated_term"]
+    examples = {data[0]["examples"][j]["acl_id"]: data[0]["examples"][j]["machine_translation"] for j in range(len(data[0]["examples"]))}
+    for i in range(1, len(data)):
+        if old_term == data[i]["term"]:
+            for row in data[i]["examples"]:
+                examples[row["acl_id"]] = row["machine_translation"]
+        else:
+            cleaned_data[old_term.strip()] = {
+                "translated_term": translated_term,
+                "examples": examples
+            }
+            if "translated_term" in data[i]:
+                translated_term = data[i]["translated_term"]
+            old_term = data[i]["term"]
+            examples = {data[i]["examples"][j]["acl_id"]: data[i]["examples"][j]["machine_translation"] for j in range(len(data[i]["examples"]))}
+
+    return cleaned_data
+    
+
+if __name__ == "__main__":
+
+    path = "./data/terms.json"
+    data = read_json(path)
+
+    paths = glob("/Users/bkhmsi/Downloads/terminologies_with_reference/*.json")
+
+    lang_to_code = {}
+    code_to_lang = {}
+    for code, langs in languages_codes:
+        for lang in langs.split(";"):
+            lang_to_code[lang.lower().strip()] = code
+            code_to_lang[code] = lang.strip()
+
+    other_data = {}
+    codes = []
+    langs = []
+    template = "'{}'"
+    for path in paths:
+        lang_code = lang_to_code[os.path.basename(path)[:-5]]    
+        other_data[lang_code] = clean_data(read_json(path))
+        codes += [template.format(lang_code)]
+        langs += [template.format(code_to_lang[lang_code])]
+
+    print(', '.join(codes))
+    print(', '.join(langs))
+
+    
+    template = "<div class='{}_term' style='display:none;'> {{{{ .{}_term }}}} </div>"
+    for lang_code in other_data:
+        print(template.format(lang_code, lang_code))
+
+    exit()
+
+    for i, row in tqdm(enumerate(data[:1000]), total=1000):
+        term = row["term"]
+        for lang in other_data:
+            if term not in other_data[lang]: continue
+            data[i][f"{lang}_term"] = other_data[lang][term]["translated_term"]
+            for j in range(min(len(other_data[lang][term]["examples"]), len(data[i]["sentences"]))):
+                acl_id = data[i]["sentences"][j]["id"].split(":")[0].strip()
+                if acl_id not in other_data[lang][term]["examples"]:
+                    continue 
+                data[i]["sentences"][j][f"{lang}_sentence"] = other_data[lang][term]["examples"][acl_id]
+    
+    write_json("terms_16langs.json", data)
